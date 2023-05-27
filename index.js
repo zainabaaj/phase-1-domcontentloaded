807da4827506529650e0e9770e5bf8d9724cd0a4
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-27 16:23:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const paragraph = this.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+
+});
